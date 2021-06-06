b9acc9f023d48ba58abb8f435833fc0eefb4415b
OrbenMichele <micheleorben@gmail.com>
2021-06-05 21:30:07 -0300

    Added JUnit tests with Gradle

---
+++ src/main/java/springframework/Greeting.java
@@ -0,0 +1,19 @@
+package springframework;
+
+/**
+ * Created by micheleorben on 6/5/21
+ */
+
+public class Greeting {
+
+    private static final String HELLO = "Hello";
+    private static final String WORLD = "World";
+
+    public String helloWorld(){
+        return HELLO + " " + WORLD;
+    }
+
+    public String helloWorld(String name){
+        return HELLO + " " + name;
+    }
+}
